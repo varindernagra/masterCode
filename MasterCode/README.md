6bca08a5e75fda7e8f17549eaac06b9bea748d58
varindernagra <varindernagra@gmail.com>
2014-09-15 00:57:01 -0700

    MasterCodeForGit

---
+++ MasterCode/README.md
@@ -0,0 +1,8 @@
+MagicMirror
+===========
+
+The super magic interface of my personal Magic Mirror. More information about this project can be found on my [blog](http://michaelteeuw.nl/tagged/magicmirror).
+
+Runs as a php script on a web server with basically no external dependencies. Can use socket.io for XBEE integration, but isn't required for basic functionality.
+
+Modify js/config.js to change some general variables (language, wather location, compliments, news feed RSS) and calendar.php to add your own ICS calendar
---
+++ MasterCode/js/config.js
@@ -0,0 +1,33 @@
+// for navigator language
+//var lang = window.navigator.language;
+// you can change the language
+var lang = 'en';
+
+//change weather params here:
+//units: metric or imperial
+var weatherParams = {
+    'q':'Fremont,US',
+    //'q':'Baarn,Netherlands',
+    'units':'imperial',
+    //'units':'metric',
+    'lang':lang
+};
+
+//var feed = 'http://feeds.nos.nl/nosjournaal?format=rss';
+//var feed = 'http://www.nu.nl/feeds/rss/achterklap.rss';
+//var feed = 'http://www.nu.nl/feeds/rss/opmerkelijk.rss';
+var feed = 'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml';
+
+// compliments:
+/*
+var compliments = [
+            //'Hey, handsome!',
+            //'Hi, sexy!',
+            //'Hello, beauty!',
+            //'You look sexy!',
+            //'Wow, you look hot!',
+            'sexxyyyy!',
+            'working like a charm!',
+            'XoXo'
+        ];
+*/
